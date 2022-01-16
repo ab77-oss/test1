f29f123ebd7acb29c1e423a1e92dde79bb744d8f
abdelouahed elyahiaoui <abdelouahed4@yahoo.fr>
2022-01-16 19:38:32 +0100

    Gulp

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,7 @@
 'use strict';
 module.export =function(grunt){
-    require('time-grunt')(grunt);
-    require('jit-grunt')(grunt);
+    require('time-grunt;')(grunt);
+    require('jit-grunt;')(grunt);
     grunt.initConfig({
         sass:{
             dist:{
@@ -30,9 +30,80 @@ module.export =function(grunt){
                     }
                 }
             }
+        },
+        copy:{
+            html:{
+                files:[{
+                    expand:true,
+                    dot:true,
+                    cwd:'./',
+                    src:['*.ht'],
+                    dest:'dist'
+                }]
+            },
+            fonts:{
+                files:[{
+                    expand:true,
+                    dot:true,
+                    cwd:'node_modules/font-awesome',
+                    src:['font/*.*'],
+                    dest:'dist'
+                }]
+            }
+        },
+        clean:{
+            build:{
+                src:['dist/']
+            }
+        },
+        imagemin:{
+            dynamic:{
+                files:[{
+                    expand:true,
+                    dot:true,
+                    cwd:'./',
+                    src:['img/*.{png, jpg, gif}'],
+                    dest:'dist/'
+                }]
+            }
+        },
+        useminPrepare:{
+            foo:{
+                dest:'dist',
+                src:['contactus.html', 'aboutus.html', 'index.html']
+            },
+            options:{
+                flow:{
+                    steps:{
+                        css:['cssmin'],
+                        js:['uglify']
+                    },
+                    post:{
+                        css:[{
+                            name:'cssmin',
+                            createConfig:function(context, block){
+                                var generated=context.options.generated;
+                                generated.options={
+                                    keepSpecialComments:0, rebase:false
+                                };
+                            }
+                        }]
+                    }
+                }
+            }
         }
     });
     grunt.registerTask('css',['sass']);
     grunt.registerTask('default',['browserSync','watch']);
+    grunt.registerTask('build',[
+        'clean',
+        'copy',
+        'imagemin'
+    ]);
+
+
+
+
+
 };
 
